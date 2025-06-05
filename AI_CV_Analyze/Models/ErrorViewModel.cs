027245c54a567e78008fc38f80e2663d265a6c04
okean8901 <trangialoc892001@gmail.com>
2025-06-05 10:10:41 +0700

    update chức năng phân tích CV

--- AI_CV_Analyze/Models/ErrorViewModel.cs
+++ AI_CV_Analyze/Models/ErrorViewModel.cs
@@ -2,8 +2,8 @@ namespace AI_CV_Analyze.Models
 {
     public class ErrorViewModel
     {
-        public string? RequestId { get; set; }
-
+        public string RequestId { get; set; }
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public string Message { get; set; }
     }
 }
